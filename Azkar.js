eb5274a1077086aca33fe6cc1e4bc520876ef6d6
Abdalrahman Maher <104376067+Abdomaher01@users.noreply.github.com>
2022-07-15 14:11:43 +0200

    Add files via upload

---
+++ Azkar.js
@@ -0,0 +1,110 @@
+document.getElementById("myrange").addEventListener('change',function change_font_size(){
+    var myrange=document.getElementById("myrange");
+    if(myrange.value>90){
+        var body=document.getElementById("body");
+        body.style.fontSize = "xxx-large";
+    }
+    else if(myrange.value<90 && myrange.value>72){
+        var body=document.getElementById("body");
+        body.style.fontSize = "xx-large";
+    }
+    else if(myrange.value<72 && myrange.value>54){
+        var body=document.getElementById("body");
+        body.style.fontSize = "x-large";
+    }
+    else if(myrange.value<54 && myrange.value>36){
+        var body=document.getElementById("body");
+        body.style.fontSize = "larger";
+    }
+    else if(myrange.value<36 && myrange.value>16){
+        var body=document.getElementById("body");
+        body.style.fontSize = "large";
+    }
+    else{
+        var body=document.getElementById("body");
+        body.style.fontSize = "medium";
+    }
+})
+
+var counter = 0;
+function decrementdiv(sectionid){
+    var stringlength=sectionid.length;
+    if(stringlength==8){
+        var buttonid=sectionid.slice(-1);
+    }
+    else{
+        var steps=stringlength-8;
+        steps++;
+        var buttonid=sectionid.slice(-(steps),);
+    }
+    var button="button"+buttonid;
+    var strvalue=document.getElementById(button).value;
+    var value=parseInt(strvalue);
+    if(value>0){
+        value-=1;
+        document.getElementById(button).value= value;
+        if(value==0){
+            counter++;
+            if (counter==document.getElementById("checkempty").children.length){
+                alert("بارك الله فيك")
+        }
+      }
+    }
+}
+function refresh_element(value,buttonid){
+    if (document.getElementById(buttonid).value==0){
+        counter-=1
+    }
+    if(value=="ten"){
+        document.getElementById(buttonid).value=11;
+    }
+    else if(value=="one"){
+        document.getElementById(buttonid).value=2;
+    }
+    else if(value=="three"){
+        document.getElementById(buttonid).value=4;
+    }
+    else if(value=="four"){
+        document.getElementById(buttonid).value=5;
+    }
+    else if(value=="seven"){
+        document.getElementById(buttonid).value=8;
+    }
+    else{
+        document.getElementById(buttonid).value=101;
+    }
+
+
+}
+document.getElementById('tahleel_times').addEventListener("change",function changevalue(){
+    var select=document.getElementById("tahleel_times");
+    var strvalue=select.options[select.selectedIndex].value;
+    var value=parseInt(strvalue);
+    document.getElementById("button24").value = value+1;
+    if (value==100){
+        document.getElementById("refresh_tahleel").setAttribute("onclick","javascript: refresh_element('hundred','button24')");
+        document.getElementById("tahleel").innerHTML= "مئة مرة";
+        document.getElementById("tahleel").className= "hundred";
+    }
+    else{
+        document.getElementById("refresh_tahleel").setAttribute("onclick","javascript: refresh_element('ten','button24')");
+        document.getElementById("tahleel").innerHTML= "عشر مرات";
+        document.getElementById("tahleel").className= "ten";
+    }
+})
+document.getElementById('tasbeh_times').addEventListener("change",function changevalue(){
+    var select=document.getElementById("tasbeh_times");
+    var strvalue=select.options[select.selectedIndex].value;
+    var value=parseInt(strvalue);
+    document.getElementById("button25").value = value+1;
+    if (value==100){
+        document.getElementById("refresh_tasbeh").setAttribute("onclick","javascript: refresh_element('hundred','button25')");
+        document.getElementById("tasbeh").innerHTML= "مئة مرة";
+        document.getElementById("tasbeh").className= "hundred";
+    }
+    else{
+        document.getElementById("refresh_tasbeh").setAttribute("onclick","javascript: refresh_element('ten','button25')");
+        document.getElementById("tasbeh").innerHTML= "عشر مرات";
+        document.getElementById("tasbeh").className= "ten";
+    }
+})
